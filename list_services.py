f2aa57dd9bdb97610dac7fc538d03eda8fdd68f3
voite1 <voitel@gmail.com>
2015-01-28 15:12:06 -0800

    Create list_services.py
    Extra assignment: lists services on a local computer for a range of ports specified.

---
+++ list_services.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+
+def list_services(start=1000, end=3000):
+    ''' Provide a list of ports for a specified range
+    usage: list_services(start, end) '''
+    
+    # check for input boundaries
+    if (start < 1 or end > 65535):
+        print "Socket range should fall between 1 and 65535"
+        sys.exit(0)
+    
+    # Print services
+    for i in range(start,end+1):
+        try:
+            service = socket.getservbyport(i)
+            print "Port:", i, ", Service:", service
+        except Exception:
+            # If service running
+            pass
+    
+    # Sending success signal
+    sys.exit(0)
+
+if __name__ == "__main__":
+    list_services()
